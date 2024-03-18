4266eee2cc14a9e081e4ab44f98394de3df9e9f4
rofinp <rofinugraha.dev@gmail.com>
2024-03-19 02:15:25 +0700

    Refactored code & passed the postman tests

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,30 @@
 import { nanoid } from 'nanoid';
 import books from './books.js';
 
+const serverErrorHandler = (res, error) => {
+  console.error(error.message);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Terjadi kesalahan pada server',
+  });
+};
+
 export const addBookHandler = (req, res) => {
   try {
+    const { name, readPage, pageCount } = req.body;
+    if (!name) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. Mohon isi nama buku',
+      });
+    }
+    if (readPage > pageCount) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+      });
+    }
+
     const book = {
       id: nanoid(16),
       insertedAt: new Date().toISOString(),
@@ -10,19 +32,6 @@ export const addBookHandler = (req, res) => {
       finished: req.body.pageCount === req.body.readPage,
       ...req.body,
     };
-
-    if (!book.name) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. Mohon isi nama buku',
-      });
-    }
-    if (book.readPage > book.pageCount) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-      });
-    }
 
     books.push(book);
 
@@ -34,43 +43,44 @@ export const addBookHandler = (req, res) => {
       },
     });
   } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({
-      status: 'error',
-      message: 'Terjadi kesalahan pada server',
-    });
+    return serverErrorHandler(res, error);
   }
 };
 
 export const getBooksHandler = (req, res) => {
-  const { name, reading, finished } = req.query;
+  try {
+    const { name: qName, reading, finished } = req.query;
 
-  // Create a copy to avoid modifying original data
-  let filteredBook = books.slice();
+    // Create a copy to avoid modifying original data
+    let filteredBook = books.slice();
 
-  filteredBook = filteredBook.filter((book) => {
-    const nameMatch = name ? book.name.toLowerCase().includes(name.toLowerCase()) : true;
-    const readingMatch = reading ? book.reading === Boolean(Number(reading)) : true;
-    const finishedMatch = finished ? book.finished === Boolean(Number(finished)) : true;
-    return nameMatch && readingMatch && finishedMatch;
-  });
+    filteredBook = filteredBook.filter((book) => {
+      const nameMatch = qName ? book.name.toLowerCase().includes(qName.toLowerCase()) : true;
+      const readingMatch = reading ? book.reading === Boolean(Number(reading)) : true;
+      const finishedMatch = finished ? book.finished === Boolean(Number(finished)) : true;
+      return nameMatch && readingMatch && finishedMatch;
+    });
 
-  const formattedBook = filteredBook.map((book) => ({
-    id: book.id,
-    name: book.name,
-    publisher: book.publisher,
-  }));
+    const formattedBook = filteredBook.map(({ id, name, publisher }) => ({
+      id,
+      name,
+      publisher,
+    }));
 
-  return res.status(200).json({
-    status: 'success',
-    data: { books: formattedBook },
-  });
+    console.log(books);
+
+    return res.status(200).json({
+      status: 'success',
+      data: { books: formattedBook },
+    });
+  } catch (error) {
+    return serverErrorHandler(res, error);
+  }
 };
 
 export const getBookHandler = (req, res) => {
   try {
     const { bookId } = req.params;
-
     if (!bookId || !bookId.trim()) {
       return res.status(400).json({
         status: 'fail',
@@ -95,18 +105,13 @@ export const getBookHandler = (req, res) => {
       data: { book },
     });
   } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({
-      status: 'error',
-      message: 'Terjadi kesalahan pada server',
-    });
+    return serverErrorHandler(res, error);
   }
 };
 
 export const updateBookHandler = (req, res) => {
   try {
     const { bookId } = req.params;
-
     if (!bookId || !bookId.trim()) {
       return res.status(400).json({
         status: 'fail',
@@ -114,54 +119,50 @@ export const updateBookHandler = (req, res) => {
       });
     }
 
-    if (!req.body.name) {
+    const index = books.findIndex((book) => book.id === bookId);
+    if (index === -1) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan',
+      });
+    }
+
+    const {
+      name, readPage, pageCount,
+    } = req.body;
+    if (!name) {
       return res.status(400).json({
         status: 'fail',
         message: 'Gagal memperbarui buku. Mohon isi nama buku',
       });
     }
 
-    if (req.body.readPage > req.body.pageCount) {
+    if (readPage > pageCount) {
       return res.status(400).json({
         status: 'fail',
         message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
       });
     }
 
-    const index = books.findIndex((book) => book.id === bookId);
-
-    if (index === -1) {
-      res.status(404).json({
-        status: 'fail',
-        message: 'Gagal memperbarui buku. Id tidak ditemukan',
-      });
-    }
-
-    const updatedBook = {
+    books[index] = {
       ...books[index],
       updatedAt: new Date().toISOString(),
+      finished: pageCount === readPage,
       ...req.body,
     };
-
-    books[index] = updatedBook;
-
+    console.log(books[index]);
     return res.status(200).json({
       status: 'success',
       message: 'Buku berhasil diperbarui',
     });
   } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({
-      status: 'error',
-      message: 'Terjadi kesalahan pada server',
-    });
+    return serverErrorHandler(res, error);
   }
 };
 
 export const deleteBookHandler = (req, res) => {
   try {
     const { bookId } = req.params;
-
     if (!bookId || !bookId.trim()) {
       return res.status(400).json({
         status: 'fail',
@@ -170,7 +171,6 @@ export const deleteBookHandler = (req, res) => {
     }
 
     const index = books.findIndex((book) => book.id === bookId);
-
     if (index === -1) {
       return res.status(404).json({
         status: 'fail',
@@ -185,10 +185,6 @@ export const deleteBookHandler = (req, res) => {
       message: 'Buku berhasil dihapus',
     });
   } catch (error) {
-    console.error(error.message);
-    return res.status(500).json({
-      status: 'error',
-      message: 'Terjadi kesalahan pada server',
-    });
+    return serverErrorHandler(res, error);
   }
 };
